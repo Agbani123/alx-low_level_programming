71213390476809aaa2f27bded422b0ac3db03852
Chidinma <okaforjulia2016@yahoo.com>
2022-09-08 17:02:12 -0700

    sizeof

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ *Return: Always 0(Success)
+ */
+
+int main(void)
+{
+char chartype;
+int inttype;
+int longinttype;
+long longlonginttype;
+float floattype;
+
+printf("Size of a char: %zu bytes \n", sizeof(chartype));
+printf("Size of an int: %zu bytes \n", sizeof(inttype));
+printf("Size of a long int: %zu bytes \n", sizeof(longinttype));
+printf("Size of a long long int: %zu bytes \n", sizeof(longlonginttype));
+printf("Size of a float: %zu bytes \n", sizeof(floattype));
+return (0);
+}
