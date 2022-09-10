e7c7c2d2f74f2be30bc13ca1caab27ca2dee6611
Chidinma <okaforjulia2016@yahoo.com>
2022-09-09 21:40:24 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Print alphabets in lowercase
+ *Return:always 0
+ */
+int main(void)
+{
+char ch;
+putchar("Alphabets from a-z are: \n");
+for (ch = 'a'; ch <= 'z'; ch = ++)
+{
+putchar("%c\n", ch);
+}
+return (0);
+}
