eb4e5ef707bc2d172c3691537ef514b7f2d21499
Chidinma <okaforjulia2016@yahoo.com>
2022-09-11 07:13:09 -0700

    worked yet again

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,14 +8,14 @@
 
 int main(void)
 {
-char ch;
-for (ch = "a", ch <= "z", ch++)
+char c;
+ for (c = "a"; c <= "z"; c++)
 {
-putchar(ch);
+putchar(c);
 }
-for (ch = "A", ch <="Z", ch++)
+ for (c = "A"; c <= "Z"; c++)
 {
-putchar(ch);
+putchar(c);
 }
 putchar("\n");
 return (0);
