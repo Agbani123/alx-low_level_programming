183430055694221da357e33a16173a64c5f89c96
Chidinma <okaforjulia2016@yahoo.com>
2022-09-12 01:41:25 -0700

    getting a hang of c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 /**
- * main- All single digit numbers of base 10 starting from 0
+ * main- numbers of base 16 in lowercase
  * Return: always 0
  */
 
 int main(void)
 {
 int a;
+char ch;
 for (a = 0; a < 10; a++)
 putchar(a + '0');
+for (ch = 'a'; ch <= 'f'; ch++)
+putchar(ch);
 putchar('\n');
 return (0);
 }
