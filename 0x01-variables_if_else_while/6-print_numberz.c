cb9ce01c729c0f72f232b7bffee3fc2f3b67a178
Chidinma <okaforjulia2016@yahoo.com>
2022-09-12 01:25:56 -0700

    numbers in base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main- All single digit numbers of base 10 starting from 0
+ * Return: always 0
+ */
+
+int main(void)
+{
+int a;
+for (a = 0; a < 10; a++)
+putchar(a);
+putchar('\n');
+return (0);
+}
